79e71dc8644c8a4ce2c257ca98737b3e73ca1f87
Benjamin Lavian <benylavian@gmail.com>
2025-02-12 23:50:39 -0500

    modified the function to only update the cells after calculating the neighbors

--- game.py
+++ game.py
@@ -25,26 +25,31 @@ def print_grid(grid):
         printout += "|\n"
         printout += vertical_divider
     print(printout)
-    
+
 def find_next_iteration(grid):
+    cells_to_kill = []
+    cells_to_regenerate = []
     # loop through the grid
     for x_idx, x in enumerate(grid):
         for y_idx, y in enumerate(x):
             num_neighbors = find_neigbors(x_idx, y_idx)
-            #print(f'{(x_idx, y_idx)} has {num_neighbors} neighbors')
             if  y == 'X':
                 if num_neighbors < 2 or num_neighbors > 3:
-                    grid[x_idx][y_idx] = 0
-            else: # otherwose y is not x and is a dead cell
+                    cells_to_kill.append((x_idx, y_idx))
+            else: # otherwise y is not x and is a dead cell
                     if num_neighbors == 3:
-                         grid[x_idx][y_idx] = 'X'
+                        cells_to_regenerate.append((x_idx, y_idx))
+    for cell in cells_to_kill:
+        grid[cell[0]][cell[1]] = 0
+    for cell in cells_to_regenerate:
+        grid[cell[0]][cell[1]] = 'X'
     print_grid(grid)
     
                
 
 
 def find_neigbors(x, y):
-    return 4
+    return 3
 
 g = generate_grid(4,4, [(2,2), (1,3), (0,0), (0,1)])
 print(g)
