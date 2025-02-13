187ceac8d79e3ff9cd6d200d49517e2d67fca7a4
Benjamin Lavian <benylavian@gmail.com>
2025-02-12 23:33:59 -0500

    find next iteration function

--- game.py
+++ game.py
@@ -26,13 +26,32 @@ def print_grid(grid):
         printout += vertical_divider
     print(printout)
     
+def find_next_iteration(grid):
+    # loop through the grid
+    for x_idx, x in enumerate(grid):
+        for y_idx, y in enumerate(x):
+            num_neighbors = find_neigbors(x_idx, y_idx)
+            #print(f'{(x_idx, y_idx)} has {num_neighbors} neighbors')
+            if  y == 'X':
+                if num_neighbors < 2 or num_neighbors > 3:
+                    grid[x_idx][y_idx] = 0
+            else: # otherwose y is not x and is a dead cell
+                    if num_neighbors == 3:
+                         grid[x_idx][y_idx] = 'X'
+    print_grid(grid)
+    
+               
 
-        
+
+def find_neigbors(x, y):
+    return 4
 
 g = generate_grid(4,4, [(2,2), (1,3), (0,0), (0,1)])
 print(g)
-print(print_grid(g))
+print_grid(g)
+find_next_iteration(g)
 
 
 
+    
 
