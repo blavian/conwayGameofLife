6d8336d74fada835af55e9bc23a91a6d3e28d1a1
Benjamin Lavian <benylavian@gmail.com>
2025-04-03 20:16:39 -0400

    finished with the class implementation

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import time
+import os
 class Game:
     def __init__(self,rows, columns, initial_living_cells):
         self.rows = rows
@@ -63,8 +65,37 @@ class Game:
                 neighbor_count += 1
         return neighbor_count
     
+    def find_next_iteration(self):
+        cells_to_kill = []
+        cells_to_regenerate = []
+        # loop through the grid
+        for x_idx, x in enumerate(self.grid):
+            for y_idx, y in enumerate(x):
+                num_neighbors = self.find_neigbors(x_idx, y_idx)
+                # print(f'There are {num_neighbors} neighbors for ({x_idx}, {y_idx})')
+                if y == "X":
+                    if num_neighbors and num_neighbors < 2 or num_neighbors > 3:
+                        cells_to_kill.append((x_idx, y_idx))
+                else:  # otherwise y is not x and is a dead cell
+                    if num_neighbors and num_neighbors == 3:
+                        cells_to_regenerate.append((x_idx, y_idx))
+        for cell in cells_to_kill:
+            self.grid[cell[0]][cell[1]] = 0
+        for cell in cells_to_regenerate:
+            self.grid[cell[0]][cell[1]] = "X"
+        self.generate_grid()
+    
 
-g = Game(3,3,[(0,1), (1,2)])
-print(g)
-print(g.find_neigbors(0,1))
+
+
+
+if __name__ == "__main__":
+    g = Game(7, 7, [(4, 2), (4, 3), (4, 4), (3, 4), (2,3)])
+    print(g)
+    previous_grid = g.grid
+    while any('X' in column for column in g.grid):
+       time.sleep(2)
+       os.system('clear')
+       g.find_next_iteration()
+       print(g)
 
